f430fcdc1c424e3740f432396df3c538698495ef
tanujn45 <tanujn45@gmail.com>
2020-10-11 22:33:37 +0530

    own sequence

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ $("document").ready(function () {
   const canvas = document.getElementById("hero-lightpass");
   const context = canvas.getContext("2d");
 
-  const frameCount = 147;
+  const frameCount = 480;
   const currentFrame = (index) =>
-    `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+    `jpg/${index
       .toString()
       .padStart(4, "0")}.jpg`;
 
