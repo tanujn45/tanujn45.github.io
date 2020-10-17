6306a3c4a06b0d544ce6981c5a8c21485ee70b07
tanujn45 <tanujn45@gmail.com>
2020-10-17 12:15:50 +0530

    changes

--- app.js
+++ app.js
@@ -1,67 +1,16 @@
-/*
-const intro = document.querySelector(".intro");
-const video = intro.querySelector("video");
-const text = intro.querySelector("h1");
-
-const section = document.querySelector("section");
-const end = document.querySelector(".one");
-
-const controller = new ScrollMagic.Controller();
-
-const scene = new ScrollMagic.Scene({
-  duration: 6000,
-  triggerElement: intro,
-  triggerHook: 0,
-})
-  .addIndicators()
-  .setPin(intro)
-  .addTo(controller);
-
-const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-  duration: 2000,
-  triggerElement: intro,
-  triggerHook: 0,
-})
-  .addIndicators({
-    name: "text",
-    colorTrigger: "green",
-    indent: 200,
-  })
-  .setTween(textAnim)
-  .addTo(controller);
-
-let accelamount = 0.2;
-let scrollpos = 0;
-let delay = 0;
-
-scene.on("update", (e) => {
-  scrollpos = e.scrollPos / 1000;
-});
-
-setInterval(() => {
-  delay += (scrollpos - delay) * accelamount;
-  video.currentTime = scrollpos;
-}, 120);
-*/
-
 $("document").ready(function () {
   var controller = new ScrollMagic.Controller();
 
   $(".sample").each(function () {
-    console.log(this.children[1].children[0]);
-
-    var Scene = new ScrollMagic.Scene({
-      triggerElement: this.children[0],
-      duration: 400,
+     var Scene = new ScrollMagic.Scene({
+       triggerElement: this.children[0],
+       duration: 400,
       triggerHook: 0,
-    })
-      .setClassToggle(this, "fade-in")
+    }).setClassToggle(this, "fade-in")
       .setPin(this.children[1].children[0])
       // .addIndicators()
-      .addTo(controller);
-  });
+     .addTo(controller);
+   });
 
   const html = document.documentElement;
   const canvas = document.getElementById("hero-lightpass");
